d1678761a6d635880b02e3324a99cb71da0b0a62
yjh920127 <yjh920127@gmail.com>
2022-03-19 16:00:08 +0900

    #BACK : response Dto, swaggerConfig 추가  - 결과 값 리턴을 위한 response Dto 추가  - swagger 사용을 위한 swaggerConfig 추가

---
+++ backend/src/main/java/com/dotto/app/dto/response/Failure.java
@@ -0,0 +1,10 @@
+package com.dotto.app.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Failure implements Result{
+    private String msg;
+}
