2fade8ed9545b483ad2dfc48198790506997cbcc
yjh920127 <yjh920127@gmail.com>
2022-06-01 20:34:39 +0900

    feat : initDB - initMember 추가

--- backend/src/main/java/com/dotto/app/InitDB.java
+++ backend/src/main/java/com/dotto/app/InitDB.java
@@ -1,7 +1,10 @@
 package com.dotto.app;
 
+import com.dotto.app.entity.member.Member;
 import com.dotto.app.entity.member.Role;
 import com.dotto.app.entity.member.RoleType;
+import com.dotto.app.exception.RoleNotFoundException;
+import com.dotto.app.repository.member.MemberRepository;
 import com.dotto.app.repository.role.RoleRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,12 +24,13 @@ import java.util.stream.Collectors;
 public class InitDB {
 
     private final RoleRepository roleRepository;
+    private final MemberRepository memberRepository;
 
     @EventListener(ApplicationReadyEvent.class)
     @Transactional
     public void initDB(){
         initRole();
-
+        initMember();
         log.info("initialize database");
     }
 
@@ -35,4 +39,18 @@ public class InitDB {
                 List.of(RoleType.values()).stream().map(roleType -> new Role(roleType)).collect(Collectors.toList())
         );
     }
+
+    private void initMember(){
+        memberRepository.saveAll(
+                List.of(new Member("member1", "1234", "nickname", "male", "01012345678"
+                                , List.of(roleRepository.findByRoleType(RoleType.ROlE_NORMAL).orElseThrow(RoleNotFoundException::new))),
+                        new Member("member2", "1234", "nickname2", "female", "01012345678"
+                                , List.of(roleRepository.findByRoleType(RoleType.ROLE_ARTIST).orElseThrow(RoleNotFoundException::new))
+                        ),
+                        new Member("member3", "1234", "nickname3", "none", "01012345678"
+                                ,List.of(roleRepository.findByRoleType(RoleType.ROLE_ADMIN).orElseThrow(RoleNotFoundException::new))
+                        )
+                )
+        );
+    };
 }
