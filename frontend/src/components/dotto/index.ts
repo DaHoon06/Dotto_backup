de50fa53c7c3155f52e11ec74d7b7c28780e291a
dahoon06 <dahoon226@naver.com>
2022-06-20 18:27:28 +0900

    feat(ISSUES #51, #52, #39): 닷투 게시판 댓글 / 리뷰 컴포넌트화 작업 중

--- frontend/src/components/dotto/index.ts
+++ frontend/src/components/dotto/index.ts
@@ -1,13 +1,19 @@
 import DottoComponent from "@/components/dotto/DottoComponent.vue";
 import DottoPostingComponent from "@/components/dotto/DottoPostingComponent.vue";
 import DottoPostingButton from "@/components/dotto/DottoPostingButton.vue";
-import DottoComponentContainer from "@/components/dotto/DottoComponentContainer.vue";
+import DottoContainer from "@/components/dotto/DottoContainer.vue";
 import DottoDetailComponent from "@/components/dotto/DottoDetailComponent.vue";
+import DottoCommentComponent from "@/components/dotto/tab/DottoCommentComponent.vue";
+import DottoReviewComponent from "@/components/dotto/tab/DottoReviewComponent.vue";
+import DottoTabsContainer from "@/components/dotto/tab/DottoTabsContainer.vue";
 
 export {
   DottoComponent,
   DottoPostingComponent,
   DottoPostingButton,
-  DottoComponentContainer,
+  DottoContainer,
   DottoDetailComponent,
+  DottoCommentComponent,
+  DottoReviewComponent,
+  DottoTabsContainer
 }
---
+++ frontend/src/components/dotto/tab/DottoTabsContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { DottoCommentComponent, DottoReviewComponent } from "@/components/dotto";
+
+@Component({
+  DottoCommentComponent,
+  DottoReviewComponent
+})
+export default class DottoTabsContainer extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
