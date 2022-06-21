27d507eb2100439dba5b9b5ad440c51839ba8555
yjh920127 <yjh920127@gmail.com>
2022-06-22 01:13:05 +0900

    feat : 닷투게시판 - 게시글 생성시 작성자 정보를 받지않고 직접 주입하는 AOP - @interface

---
+++ backend/src/main/java/com/dotto/app/aop/AssignMemberNo.java
@@ -0,0 +1,11 @@
+package com.dotto.app.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(value = RetentionPolicy.RUNTIME)
+@Target(value = ElementType.METHOD)
+public @interface AssignMemberNo {
+}
