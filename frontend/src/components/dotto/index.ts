0cb6fe7427bc680623c58daadffe9af25d39bf8a
dahoon06 <dahoon226@naver.com>
2022-06-21 16:41:54 +0900

    design: 닷투 게시판 상세 보기

--- frontend/src/components/dotto/index.ts
+++ frontend/src/components/dotto/index.ts
@@ -3,9 +3,6 @@ import DottoPostingComponent from "@/components/dotto/DottoPostingComponent.vue"
 import DottoPostingButton from "@/components/dotto/DottoPostingButton.vue";
 import DottoContainer from "@/components/dotto/DottoContainer.vue";
 import DottoDetailComponent from "@/components/dotto/DottoDetailComponent.vue";
-import DottoCommentComponent from "@/components/dotto/tab/DottoCommentComponent.vue";
-import DottoReviewComponent from "@/components/dotto/tab/DottoReviewComponent.vue";
-import DottoTabsContainer from "@/components/dotto/tab/DottoTabsContainer.vue";
 
 export {
   DottoComponent,
@@ -13,7 +10,4 @@ export {
   DottoPostingButton,
   DottoContainer,
   DottoDetailComponent,
-  DottoCommentComponent,
-  DottoReviewComponent,
-  DottoTabsContainer
 }
