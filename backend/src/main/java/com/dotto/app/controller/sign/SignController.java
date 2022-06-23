d3aed902b8a46e48d719c0f02314f44f3b7b4c42
유재현 <yjh920127@gmail.com>
2022-06-23 15:03:00 +0900

    feat : SignController - swagger-ui @어노테이션 추가

--- backend/src/main/java/com/dotto/app/controller/sign/SignController.java
+++ backend/src/main/java/com/dotto/app/controller/sign/SignController.java
@@ -4,6 +4,8 @@ import com.dotto.app.dto.response.Response;
 import com.dotto.app.dto.sign.SignInRequest;
 import com.dotto.app.dto.sign.SignUpRequest;
 import com.dotto.app.service.sign.SignService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import javax.validation.Valid;
 
 import static com.dotto.app.dto.response.Response.success;
 
+@Api(value = "Sign Controller" ,tags ="sign" )
 @RestController
 @RequiredArgsConstructor
 public class SignController {
@@ -19,6 +22,7 @@ public class SignController {
     private final SignService signService;
 
 
+    @ApiOperation(value = "회원가입", notes = "회원가입 요청을 한다")
     @PostMapping("/api/sign-up")
     @ResponseStatus(HttpStatus.CREATED)
     public Response SignUp( @RequestBody SignUpRequest req){
@@ -26,12 +30,14 @@ public class SignController {
         return success();
     }
 
+    @ApiOperation(value = "로그인", notes = "로그인 요청을 한다")
     @PostMapping("/api/sign-in")
     @ResponseStatus(HttpStatus.OK)
     public Response SignIn(@RequestBody SignInRequest req){
         return success(signService.SignIn(req));
     }
 
+    @ApiOperation(value = "액세스 토큰 제발급", notes = "리프레쉬 토큰을 통해 액세스토큰을 재발급 받는다")
     @PostMapping("/api/refresh-token")
     @ResponseStatus(HttpStatus.OK)
     public Response refreshTokenResponse ( @RequestHeader(value = "Authorization") String refreshToken){
