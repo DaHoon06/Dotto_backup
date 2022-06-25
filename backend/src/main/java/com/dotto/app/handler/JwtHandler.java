0a26ec597005ada4431391bbcbaa31abf5396c5b
yjh920127 <yjh920127@gmail.com>
2022-06-25 15:37:55 +0900

    chore :  토큰 bearer 삭제 - 프론트와 협의 완료

--- backend/src/main/java/com/dotto/app/handler/JwtHandler.java
+++ backend/src/main/java/com/dotto/app/handler/JwtHandler.java
@@ -13,11 +13,11 @@ import java.util.Optional;
 @Component
 public class JwtHandler {
 
-    private final String type = "Bearer ";
+//    private final String type = "Bearer ";
 
     public String createToken(String key, Map<String, Object> privateClaims, Long maxAgeSeconds){
         Date now = new Date();
-        return type + Jwts.builder()
+        return Jwts.builder()
                 .addClaims(privateClaims)
                 .addClaims(Map.of(Claims.ISSUED_AT, now, Claims.EXPIRATION, new Date(now.getTime() + maxAgeSeconds * 1000L)))
                 .signWith(SignatureAlgorithm.HS256, key.getBytes())
@@ -27,16 +27,16 @@ public class JwtHandler {
 
     public Optional<Claims> parse (String key, String token){
         try{
-            return Optional.of(Jwts.parser().setSigningKey(key.getBytes()).parseClaimsJws(unType(token)).getBody());
+            return Optional.of(Jwts.parser().setSigningKey(key.getBytes()).parseClaimsJws(token).getBody());
         }catch (JwtException e){
             return Optional.empty();
         }
     }
 
 
-    private String unType(String token){
-        return token.substring(type.length());
-    }
+//    private String unType(String token){
+//        return token.substring(type.length());
+//    }
 
 
 }
