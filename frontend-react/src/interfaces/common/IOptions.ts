6eb0554ee53876de973196f82659384990592bd9
gengjun <cockyblind@gmail.com>
2022-11-22 22:56:10 +0900

    chore: migration(interfaces, lib)

---
+++ frontend-react/src/interfaces/common/IOptions.ts
@@ -0,0 +1,10 @@
+export type IOptions = {
+  isModal: boolean
+  next: boolean
+  prev: boolean
+}
+
+export type CssControl = {
+  BLUR: boolean
+  TOPHIDE: boolean
+}
---
+++ frontend-react/src/lib/axios.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosInstance } from 'axios'
+
+//const baseURL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://back.ap.ngrok.io/api'
+const baseURL = process.env.NODE_ENV === 'development' ? '/api' : '/api'
+
+const instance: AxiosInstance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 20000,
+})
+
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken') ?? ''
+  if (token.length) {
+    config.headers!.Authorization = `Bearer ${token}`
+    return config
+  }
+  return config
+})
+
+instance.interceptors.response.use(
+  (response) => {
+    // const { data } = response
+    // const { result } = data
+    return response
+  },
+  (error) => {
+    const { response } = error
+    const { status } = response
+
+    if (status === 401) {
+      console.log('401 ERROR')
+    } else {
+      console.log('OTHER ERROR')
+    }
+    return response
+  }
+)
+export const ins = instance
