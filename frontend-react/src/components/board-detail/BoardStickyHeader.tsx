c100be33397ba0dfe83e065fdf2f43a502c5cc33
gengjun <cockyblind@gmail.com>
2022-12-11 02:34:33 +0900

    refactor: board-detail 페이지 관심사 분리

--- frontend-react/src/components/board-detail/BoardStickyHeader.tsx
+++ frontend-react/src/components/board-detail/BoardStickyHeader.tsx
@@ -1,5 +1,32 @@
-import React from 'react'
+import Typography from '../common/typography/Typography'
+import styles from './BoardStickyHeader.module.scss'
+import { ReactComponent as PinIcon } from '@/assets/icons/board/dotto/pin.svg'
+import Button from '../common/button/Button'
 
-export default function BoardStickyHeader() {
-  return <div>BoardStickyHeader</div>
+interface IBoardStickyHeader {
+  postId: string
+}
+
+export default function BoardStickyHeader({ postId }: IBoardStickyHeader) {
+  return (
+    <header className={styles.stickyHeader}>
+      <div className={styles.headerWrapper}>
+        <Typography variant="sub1" fontWeight="regular" fontColor="gray2">
+          이 작업이 마음에 드시나요?
+        </Typography>
+        <span className="flex gap-16">
+          <Button
+            className={styles.button}
+            icon={<PinIcon />}
+            variant="primary-outline"
+          >
+            찜하기
+          </Button>
+          <Button className={styles.button} variant="primary">
+            의뢰하기
+          </Button>
+        </span>
+      </div>
+    </header>
+  )
 }
