a36baadcc3d99cf31aea10e620eed23b4918e403
dahoon06 <dahoon226@naver.com>
2022-12-13 22:42:28 +0900

    feat: kakao address component 추가
    ** 모달이 렌더될 때 에러 발생 **
    
    ERROR
    TypeError: Cannot read properties of null (reading 'document') at k

--- frontend-react/src/components/register/form/TattoistForm.tsx
+++ frontend-react/src/components/register/form/TattoistForm.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import { IRegister } from '@/interfaces/register'
-import { Button } from '@/components/register/button/Button'
 import Upload from '@/assets/icons/common/upload.svg'
-import style from '@/assets/styles/common/button.module.scss'
+import {
+  IAddressInfo,
+  KakaoAddress,
+} from '@/components/register/kakao-api/KakaoAddress'
+import cn from 'classnames'
 
 export const TattoistForm = (props: IRegister.OPTIONS) => {
   const { additionalData } = props
@@ -28,24 +31,28 @@ export const TattoistForm = (props: IRegister.OPTIONS) => {
     return additionalData(additionalInfo)
   }
 
+  const addressInfo = (props: IAddressInfo) => {
+    const { address, jibunAddress } = props
+    setAdditionalInfo({
+      ...additionalInfo,
+      address: address || jibunAddress,
+    })
+  }
+
   return (
     <section className={'register-options'}>
       <section className={'register-items'}>
         <label>작업실 주소</label>
         <div className={'flex-justify-between'}>
           <input
-            onChange={sendData}
-            className={'register__input'}
+            disabled={true}
+            className={cn('register__input')}
             type={'text'}
             placeholder={'주소찾기'}
             value={address}
             name={'address'}
           />
-          <Button
-            label={'주소검색'}
-            className={style.primary__button}
-            buttonStyle={customStyle.button}
-          />
+          <KakaoAddress addressInfo={addressInfo} />
         </div>
       </section>
       <section className={'register-items mt-16'}>
---
+++ frontend-react/src/components/register/kakao-api/KakaoAddress.tsx
@@ -0,0 +1,74 @@
+import DaumPostcode from 'react-daum-postcode'
+import React, { useState } from 'react'
+import style from '@/assets/styles/common/button.module.scss'
+import { Button } from '@/components/register/button/Button'
+import './KakaoAddress.scss'
+
+export interface IAddressInfo {
+  address: string
+  jibunAddress: string
+}
+
+interface AddressProp {
+  addressInfo: (data: IAddressInfo) => void
+}
+
+export const KakaoAddress = (props: AddressProp) => {
+  const { addressInfo } = props
+  const [openPostcode, setOpenPostcode] = useState<boolean>(false)
+
+  const customStyle = {
+    button: {
+      width: '120px',
+    } as React.CSSProperties,
+  }
+
+  // 버튼 클릭 이벤트
+  const clickButton = () => {
+    setOpenPostcode((current) => !current)
+  }
+
+  // 주소 선택 이벤트
+  const selectAddress = (data: any) => {
+    const address = data.address
+    const jibunAddress = data.jibunAddress
+    const sendData = {
+      address,
+      jibunAddress,
+    }
+    addressInfo(sendData)
+    modalClose()
+  }
+
+  const modalClose = () => {
+    setOpenPostcode(false)
+  }
+
+  return (
+    <>
+      <Button
+        label={'주소검색'}
+        className={style.primary__button}
+        buttonStyle={customStyle.button}
+        onClickEvent={clickButton}
+      />
+      {openPostcode && (
+        <article className={'kakao-address--modal'}>
+          <DaumPostcode
+            onComplete={selectAddress}
+            autoClose={false}
+            defaultQuery="판교역로 235"
+            className={'address-body'}
+          />
+          <section className={'address-footer'}>
+            <Button
+              label={'닫기'}
+              className={style.black__button}
+              onClickEvent={modalClose}
+            />
+          </section>
+        </article>
+      )}
+    </>
+  )
+}
