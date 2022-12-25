c061efea3083cf82257b8baf41254088b59cc655
gengjun <cockyblind@gmail.com>
2022-12-25 19:17:52 +0900

    feature: update  router

--- frontend-react/src/router/index.tsx
+++ frontend-react/src/router/index.tsx
@@ -9,30 +9,38 @@ import MainLayout from '@/components/layout/MainLayout'
 import BoardPost from '@/pages/BoardPost'
 import BoardPostDetail from '@/pages/BoardPostDetail'
 import BoardWrite from '@/pages/BoardWrite'
+import Request from '@/pages/request/Request'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<MainLayout />}>
-      <Route index element={<Navigate to="/dotto/board" />} />
-      <Route path="dotto">
-        <Route path="board">
-          <Route index element={<Dotto />} />
-          <Route path="post" element={<BoardPost />} />
-          <Route path="view/:postNo" element={<BoardPostDetail />} />
-          <Route path="write" element={<BoardWrite />} />
+    <Route>
+      <Route element={<MainLayout />}>
+        <Route index element={<Navigate to="/dotto/board" />} />
+        <Route path="dotto">
+          <Route path="board">
+            <Route index element={<Dotto />} />
+            <Route path="post" element={<BoardPost />} />
+            <Route path="view/:postNo" element={<BoardPostDetail />} />
+            <Route path="write" element={<BoardWrite />} />
+          </Route>
+
+          <Route path="feed">
+            <Route index />
+            <Route path="post" />
+          </Route>
         </Route>
 
-        <Route path="feed">
-          <Route index />
-          <Route path="post" />
-        </Route>
+        <Route path="estimate/:postNo" />
+        <Route path="search/result/:keyword" />
+        <Route path="my" />
+        <Route path="401" />
+        <Route path="*" />
       </Route>
 
-      <Route path="estimate/:postNo" />
-      <Route path="search/result/:keyword" />
-      <Route path="my" />
-      <Route path="401" />
-      <Route path="*" />
+      <Route path="request">
+        <Route index element={<Navigate to="/*" />} />
+        <Route path=":postNo" element={<Request />} />
+      </Route>
     </Route>
   )
 )
