ee28326e5aa92fe9e74a0b73fb3002d905366048
dahoon06 <dahoon226@naver.com>
2023-08-27 23:33:41 +0900

    feat: icon svg 속성 변경

--- app/src/components/common/icons/Hamburger.tsx
+++ app/src/components/common/icons/Hamburger.tsx
@@ -4,7 +4,7 @@ import Svg, { Path } from 'react-native-svg'
 export const HamburgerIcon = () => {
   return (
     <View>
-      <Svg width="28" height="28" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <Svg width="28" height="28" viewBox="0 0 20 20" fill="none">
         <Path d="M2.5 14.1667H17.5M2.5 10H17.5M2.5 5.83337H17.5" stroke="white" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
       </Svg>
     </View>
--- app/src/components/common/icons/HomeIcon.tsx
+++ app/src/components/common/icons/HomeIcon.tsx
@@ -4,8 +4,8 @@ import Svg, { Path } from 'react-native-svg'
 export const HomeIcon = () => {
   return (
     <View>
-      <Svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <Path d="M6 10V19C6 19.2652 6.10536 19.5196 6.29289 19.7071C6.48043 19.8946 6.73478 20 7 20H17C17.2652 20 17.5196 19.8946 17.7071 19.7071C17.8946 19.5196 18 19.2652 18 19V10M6 10L12 4L18 10M6 10L4 12M18 10L20 12M10 13H14V17H10V13Z" stroke="white" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round"/>
+      <Svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+        <Path d="M6 10V19C6 19.2652 6.10536 19.5196 6.29289 19.7071C6.48043 19.8946 6.73478 20 7 20H17C17.2652 20 17.5196 19.8946 17.7071 19.7071C17.8946 19.5196 18 19.2652 18 19V10M6 10L12 4L18 10M6 10L4 12M18 10L20 12M10 13H14V17H10V13Z" stroke="white" strokeWidth="1.66667" strokeLinecap="round" strokeLinejoin="round"/>
       </Svg>
     </View>
   )
